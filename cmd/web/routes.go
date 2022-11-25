72e6c5a5fcd94b6a1a818aea06062caa65c7281d
utkarsh58 <utkarsh.b1358@gmail.com>
2022-11-25 14:35:48 +0530

    initial commit

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"github.com/itzYubi/bookings/config"
-	"github.com/itzYubi/bookings/handlers"
 	"net/http"
 
+	"github.com/itzYubi/bookings/config"
+	"github.com/itzYubi/bookings/handlers"
+
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -12,11 +14,14 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux := chi.NewRouter()
 
-	//mux.Use(middleware.Recoverer)
-	//mux.Use(NoSurf)
-	//mux.Use(SessionLoad)
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	fileserver := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileserver))
 	return mux
 }
