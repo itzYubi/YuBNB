895e8f4d57466024755ad780d606fb3cdcec6374
utkarsh58 <utkarsh.b1358@gmail.com>
2023-07-01 13:16:40 +0530

    Small changes

---
+++ migrations/20230701070915_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (first_name,last_name,email,"password",access_level,created_at,updated_at) VALUES
+('Utkarsh','Bhandari','admin@yubnb.com','$2a$12$73ui7wWXKwPq2x60lXombe4nrzSrw2h.OOTsE6/3TY3sr2bC/WHty',3,'2000-09-18 00:00:00','2000-09-18 00:00:00');
